e614a9136a9f637ddcf169dd2b3bf11a523d16ee
a_misc <>
2023-05-06 21:36:45 +0200

    very close to first prototype some bugs still left

--- hello.c
+++ hello.c
@@ -6,15 +6,16 @@ int b = 2;
 int main() {
     int num = 12345;
     int rev = 1;
-    int rem = 0;
+    int rem = 1;
 
     while (num != 0) {
+        //int test = 1;
         rem = num % 10;
         rev = rev * 10 + rem;
         num /= 10;
     }
 
-    printf("Reversed = %d \n", rev);
+    printf("////////////////////////////////////Reversed = %d \n", rev);
 
     return 0;
 }
--- test.py
+++ test.py
@@ -1,13 +1,31 @@
 import gdb
+import re
+
+regX = r"^(\s*)([a-zA-Z_][a-zA-Z0-9_]*)(\s*)(\+\=|\-\=|\*\=|\/\=|\%\=|\=)(\s*)([a-zA-Z0-9_]+|\-?[0-9]+(\.[0-9]+)?)"
+
+class lineHistory():
+    def __init__(self, line, var, value):
+        self.line = line
+        self.var = var
+        self.values = [value]
+
+    def append(self, value):
+        self.values.append(value)
+
+    def __str__(self):
+        return "line: {} var: {} values: {}".format(self.line, self.var, self.values)
+
 
 class gdbHandler():
-    lineHistory = []
+    lineHistorys = []
     globalHistory = dict()
 
     #TODO: dynamic amount of variables per line
     #TODO: get the given variables from a function call
     #TODO: does a variable get discovered even if it isn't assigned but just part of a function call?
-    #TODO: values are sometimes initialized with 0 so setting them to zero doesn't trigger a watchpoint
+    #TODO BUG: values are sometimes initialized with 0 so setting them to zero doesn't trigger a watchpoint
+    #TODO: add increment/decrement to variable finder (prefix and postfix) [https://en.wikipedia.org/wiki/Operators_in_C_and_C%2B%2B]
+
 
     def __init__(self, fName, cName):
         print("### initialising gdbHandler ###")
@@ -19,69 +37,62 @@ class gdbHandler():
     def setup(self):
         gdb.execute("file " + self.fileName)
         gdb.execute("set pagination off")
-        #gdb.execute("starti") # very begginnig of the program
-        #gdb.execute("break main") # at main loop
 
-    #might get simplified
     def setBreakPoint(self, place):
         print("### setting breakpoint ###")
-        #if int build a line break statement else just simple break
         exe = self.cName + ":" + str(place) if type(place) is int else place
         gdb.Breakpoint(exe, gdb.BP_BREAKPOINT, temporary=True)
-        gdb.execute("break " + exe)
-
+        #gdb.execute("break " + exe)
 
     def setWatchPoint(self, varName):
-        print("### setting watchpoint ###")
-
-        # have to otherwise can't get an access watchpoint
-        gdb.execute("awatch {}".format(varName))
+        gdb.Breakpoint(varName, gdb.BP_WATCHPOINT, temporary=True)
         gdb.execute("commands\nsilent\nend")
-        #watchP = gdb.Breakpoint(varName, type=gdb.WP_ACCESS, temporary=True)
 
-
-    #important if the current line doesnt have a var just step to next
-    def step(self):
-        gdb.execute("step")
-
-    def addLineToHistory(self, line):
-        self.lineHistory.append(line)
-
-    #sets all the neccesary variables
-    #does not update the current variable so access to the previous one is still possible
     def updateLocals(self):
         self.frame = gdb.selected_frame()
         self.block = self.frame.block()
         self.line = self.frame.find_sal().line
+        self.lineStr = gdb.execute("frame ", to_string=True ).split("\n")[1].split(" ",1)[1].strip()
 
     def findVarInLine(self):
-        print("### finding var in line ###")
-        for sym in self.block:
+        for sym in self.block: #if local scope
             if sym.is_variable and sym.line == self.line:
+                print("### found variable {} ###".format(sym.name))
                 self.var = sym
-                var = sym.name
-                #var = gdb.parse_and_eval(sym.name)
-                #print(sym.name, '=', var)
-                return var
-        return None #if we are on something weird like a function head
+                return sym.name
 
+        #attempt regex match
+        match = re.match(regX, self.lineStr)
+        if match:
+            res = match.group(2).strip()
+            print("### found variable with regex: {} ###".format(res))
+            return res
+
+        print("### no variable found  ###")
+        return None #if we are on something weird like a function head or not in scope
 
     def analyzeLine(self):
-        self.deleteAllBreakpoints()
+        print("### analyzing line ###")
         self.updateLocals()
-        self.findVarInLine()
-        print('found variable: ' + self.var.name)
+        foundVar = self.findVarInLine()
+        if not foundVar:
+            self.next()
+            return
+
         self.setWatchPoint(self.var.name)
         self.continueExecution()
         value = self.getVarValue(self.var.name)
-        print(self.line , self.var.name , '=' , value)
+        self.addToHistory(int(self.line), self.var.name, value)
 
-
+    def addToHistory(self, line, var, value):
+        self.lineHistorys.append(line)
+        if self.globalHistory.get(line) is None:
+            self.globalHistory[line] = lineHistory(line, var, value)
+            return
+        self.globalHistory[line].append( str(value) )
 
     def getVarValue(self, varName):
-        print("### getting var value ###")
-        res = gdb.parse_and_eval(varName)
-        return res
+        return gdb.parse_and_eval(varName)
 
     def run(self):
         print("### running program ###")
@@ -91,25 +102,40 @@ class gdbHandler():
         gdb.execute("quit")
 
     def continueExecution(self):
+        print("### continuing execution ###")
         gdb.execute("continue")
 
-    def deleteAllBreakpoints(self):
-        print("### getting current breakpoints ###")
+    def step(self):
+            gdb.execute("step")
+
+    def next(self):
+        gdb.execute("next")
+
+    # def deleteAllBreakpoints(self):
         for bp in gdb.breakpoints():
             bp.delete()
 
+    def showBreakpoints(self):
+        print("### active breakpoints ###")
+        for bp in gdb.breakpoints():
+            print(bp.expression, bp.temporary, bp.enabled, bp.location)
 
 
 if __name__ == "__main__":
     gdbHandler = gdbHandler("a.out", "hello.c")
-    gdbHandler.setBreakPoint(9)
+    gdbHandler.setBreakPoint(7)
     gdbHandler.run()
-    gdbHandler.analyzeLine()
+
     while True:
         try:
             gdbHandler.analyzeLine()
         except:
+            print("### end of program ###")
             break
+
+    print("### printing history ###")
+    for line in gdbHandler.lineHistorys:
+        print(gdbHandler.globalHistory[line])
 
     gdbHandler.quit()
 
